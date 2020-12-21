3bc8a2e304a8d098a40849c457eb397a1b4cf0ec
Aditya Kumar <64519341+kumaraditya2002@users.noreply.github.com>
2020-12-21 17:54:41 +0530

    Fixed url

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,4 @@
-let url = 'db.json';
+let url = 'Test-repo/public/db.json';
 let btns = document.getElementsByClassName('btnmy');
 Array.from(btns).forEach(item => {
     item.addEventListener('click', e => {
